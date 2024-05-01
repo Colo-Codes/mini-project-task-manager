2d00270215e384f88b0576e2ca63adcb5010b076
Damian Demasi <damian.demasi.1@gmail.com>
2024-05-01 19:54:28 +0930

    Main site structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.mainContainer}>
+      <header className={styles.headerContainer}>This is the header</header>
+      <section className={styles.toDoTasksContainer}>
+        List of TO DO tasks
+      </section>
+      <section className={styles.doingTasksContainer}>
+        List of DOING tasks
+      </section>
+      <section className={styles.doneTasksContainer}>
+        List of DONE tasks
+      </section>
+    </main>
+  );
 }
-
-export default App
